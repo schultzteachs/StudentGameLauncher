3dd55f39e3ad859dc9d2295471987510000727c7
SSSchultz <craigrobertschultz@gmail.com>
2026-06-07 15:21:04 -0400

    Completed Microsoft Tutorial on WPF apps

---
+++ WPF Tutorial Project/WpfApp1/GreetingsApp.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (HelloButton.IsChecked == true) 
+            { 
+                MessageBox.Show("Hello!"); 
+            }
+
+            else if (GoodbyeButton.IsChecked == true)
+            {
+                MessageBox.Show("Goodbye!");
+            }
+        }
+    }
+
+
+
+}
